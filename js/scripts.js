5c7dd660aae16d6cb41ca9580bff964c491cc4f8
unknown <hebamarzouk17@gmail.com>
2023-04-22 06:57:33 +0200

    feat:Create a header and nav and add functionality to both

---
+++ js/scripts.js
@@ -0,0 +1,190 @@
+// GET ELEMENTS
+
+const carousel = document.getElementsByClassName("carousel")[0];
+const carouselItems = carousel.getElementsByClassName("carousel-item");
+
+const carouselDots = document.getElementsByClassName("carousel-dots")[0];
+
+const mobileNav = document.getElementsByClassName("mobile-nav")[0];
+
+const deskNav = document.getElementsByClassName("desk-nav")[0];
+
+const navOpen = document.getElementsByClassName("menu-bar")[0];
+
+const navClose = document.getElementsByClassName("menu-close")[0];
+
+const sections = document.getElementsByClassName("section");
+
+// create links function
+((e) => {
+  //  create a fragment and ul for mobile navbar
+  const mobileFragment = document.createDocumentFragment();
+  const mobileLinksContainer = document.createElement("ul");
+  mobileLinksContainer.classList.add("mobile-nav-container");
+
+  //  create a fragment and ul for desktop navbar
+  const deskFragment = document.createDocumentFragment();
+  const deskLinksContainer = document.createElement("ul");
+  deskLinksContainer.classList.add("desk-nav-container");
+
+  // create the links  dynamically and append it to the ul created above and then addit to the fragment and push the fran=gment to the nav element in the html
+
+  // for mobile
+  for (section of sections) {
+    mobileLinksContainer.insertAdjacentHTML(
+      "beforeend",
+      `<li class='mobile-nav-item text-xbig '>
+    <a href='#${section.id}'>${section.dataset.nav}</a>
+  </li>`
+    );
+    mobileFragment.appendChild(mobileLinksContainer);
+    mobileNav.appendChild(mobileFragment);
+  }
+
+  // for desktop
+  for (section of sections) {
+    deskLinksContainer.insertAdjacentHTML(
+      "beforeend",
+      `<li class='desk-nav-item text-xbig '>
+    <a href='#${section.id}'>${section.dataset.nav}</a>
+  </li>`
+    );
+    deskFragment.appendChild(deskLinksContainer);
+    deskNav.appendChild(deskFragment);
+  }
+})();
+
+// get this after creating them
+const deskLinks = document.getElementsByClassName("desk-nav-item");
+const mobileLinks = document.getElementsByClassName("mobile-nav-item");
+
+const mobileNavContainer = document.getElementsByClassName(
+  "mobile-nav-container"
+)[0];
+
+const deskNavContainer =
+  document.getElementsByClassName("desk-nav-container")[0];
+
+// CREATE THE slides
+let interval;
+// handle the automatic header by change the active carousel item / dot repeatedly
+const takeTurns = () => {
+  for (item of carouselItems) {
+    if (!item.classList.contains("image-wait")) {
+      item.classList.add("image-wait");
+    } else {
+      item.classList.remove("image-wait");
+    }
+  }
+  for (dot of carouselDots.children) {
+    dot.classList.contains("dot-active")
+      ? dot.classList.remove("dot-active")
+      : dot.classList.add("dot-active");
+  }
+};
+
+// the click handler whitch change the active carousel item / dot by clickin dots
+const slide = (e) => {
+  console.log(e);
+  // a dot was clicked
+  clearInterval(interval);
+  let imageData = e.target.dataset.image;
+  for (item of carouselItems) {
+    if (item.id === imageData && !item.classList.contains("image-wait")) {
+      console.log(item);
+      item.classList.add("image-wait");
+    } else if (item.id !== imageData && item.classList.contains("image-wait")) {
+      console.log(item);
+      item.classList.remove("image-wait");
+    }
+
+    console.log(imageData);
+  }
+  for (dot of carouselDots.children) {
+    dot === e.target
+      ? dot.classList.add("dot-active")
+      : dot.classList.remove("dot-active");
+  }
+
+  // the function run without interaption
+  interval = setInterval(() => {
+    takeTurns();
+  }, 4000);
+};
+
+// scrollnav handle function
+const scrollNav = (e) => {
+  let scroll = window.scrollY;
+  scroll > 0
+    ? deskNav.classList.add("scrolled-nav")
+    : deskNav.classList.remove("scrolled-nav");
+};
+
+// handle the chandes that happen when scrolling
+const handleScroll = () => {
+  // to change the desk navbar when scrooling
+  scrollNav();
+
+  // holds all sections that passed the views screen, put empty placeholder to pass the empty check below
+  let current = [""];
+  for (section of sections) {
+    const rect = section.getBoundingClientRect();
+    if (
+      rect.top < window.innerHeight * 0.75 ||
+      rect.bottom < window.innerHeight
+    ) {
+      current.push(section.id);
+    }
+  }
+  console.log(current);
+  // search throught the desk/mobile links to add active class to the one that corresponse to the current viewd section
+
+  if (current.length > 0) {
+    let currentActive = current[current.length - 1];
+    for (mobLink of mobileLinks) {
+      if (mobLink.firstElementChild.hash === `#${currentActive}`) {
+        mobLink.classList.add("active-nav-item");
+      } else {
+        mobLink.classList.remove("active-nav-item");
+      }
+    }
+    for (deskLink of deskLinks) {
+      if (deskLink.firstElementChild.hash === `#${currentActive}`) {
+        deskLink.classList.add("active-nav-item");
+      } else {
+        deskLink.classList.remove("active-nav-item");
+      }
+    }
+  }
+};
+
+// ////////// EVENT LISTENERS ///////////
+// window.addEventListener("load", createLinks);
+
+//  Add click listeners to the dots
+carouselDots.addEventListener("click", slide);
+
+// open and close the mobile nav
+navOpen.addEventListener("click", () => {
+  mobileNav.classList.add("open-mobile-nav");
+});
+
+navClose.addEventListener("click", () => {
+  mobileNav.classList.remove("open-mobile-nav");
+});
+
+// add a class to the nav when scrooling
+
+window.addEventListener("scroll", handleScroll);
+
+deskNavContainer.addEventListener("click", (e) => {
+  if (e.target.nodeName === "A") {
+    for (section of sections) {
+      if (`#${section.id}` === e.target.hash) {
+        section.scrollIntoView();
+      }
+    }
+  }
+});
+
+console.log(deskLinks[4]);
